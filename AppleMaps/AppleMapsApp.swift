467f462601b728f354bbbaf97564cc8f49ce04d6
Hector <hectorgvazquez@gmail.com>
2021-12-14 22:59:28 -0600

    Initial Commit

---
+++ AppleMaps/AppleMapsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppleMapsApp.swift
+//  AppleMaps
+//
+//  Created by Hector Vazquez on 12/14/21.
+//
+
+import SwiftUI
+
+@main
+struct AppleMapsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AppleMaps/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  AppleMaps
+//
+//  Created by Hector Vazquez on 12/14/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
